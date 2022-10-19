b567906f89e5516473c80a03c11865bad7138571
kroles1 <korneliaoles@gmail.com>
2022-10-19 12:36:33 +0100

    models

--- server/models/post.js
+++ server/models/post.js
@@ -3,11 +3,39 @@ const { ObjectId } = require('mongodb')
 
 class Post {
     constructor(data) {
-        this.title = data.title;
-        this.name = data.name;
+        this.title = data.title
+        this.name = data.name
         this.body = data.body
+    }
+
+
+    static findById (id) {
+        return new Promise (async (resolve, reject) => {
+            try {
+                const db = await init();
+                let postData = await db.collection('posts').find({ _id: ObjectId(id) }).toArray()
+                let post = new Post({...postData[0], id: postData[0]._id});
+                resolve (post);
+            } catch (err) {
+                reject('Post not found');
+            }
+        });
+    }
+
+    static create(title, name, body){
+        return new Promise (async (resolve, reject) => {
+            try {
+                const db = await init();
+                let postData = await db.collection('posts').insertOne({ title, name, body })
+                let newPost = new Post(postData.ops[0]);
+                resolve (newPost);
+            } catch (err) {
+                reject('Error creating post');
+            }
+        });
     }
 }
 
 
 module.exports = Post
+
