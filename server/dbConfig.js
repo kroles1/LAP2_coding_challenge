5d7a4de2a38fa4ad90c9658aa18cf478018b7d73
kroles1 <korneliaoles@gmail.com>
2022-10-21 09:57:17 +0100

    cleaning up code

--- server/dbConfig.js
+++ server/dbConfig.js
@@ -2,7 +2,6 @@ const { MongoClient } = require('mongodb')
 const dotenv = require('dotenv')
 dotenv.config()
 const connectionUrl = process.env.DB_CONNECTION 
-// ? process.env.DB_CONNECTION : "mongodb://127.0.0.1:27017/telegraph";
 console.log(connectionUrl);
 const dbName = process.env.DB_NAME
 
