be802f7a4454d73825871ecad15e6fae96a0504f
Tca0 <taher.khtieb@gmail.com>
2022-10-21 09:57:29 +0100

    refacter code

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,54 +1,30 @@
 const Post = require("../models/post");
 
 async function getAllPosts(req, res) {
-  console.log("Hitting get all posts", req.headers.host);
-  const port = req.headers.host.slice(-4);
-  console.log(port);
   try {
     let posts = await Post.all()
-    console.log("from controllers:\n All posts: \n", posts);
     res.status(200).json(posts);
   } catch (err) {
-    console.log(err);
     res.status(400).json({err})
   }
 }
 
 async function createNewPost(req, res) {
-  console.log("Hitting post request on Controllers");
   try {
-    console.log(req.body);
     const { title, name, body } = req.body;
     let newPost = await Post.create(title, name, body);
     res.status(201).send(newPost);
   } catch (err) {
-    console.log(err);
     res.status(400).json({err})
   }
 }
 
 async function findById(req, res) {
-    console.log("Hitting find a specific post on conrollers");
     try{
-        console.log(req.params.id);
         const post = await Post.findById(req.params.id)
-        console.log(post);
         res.status(302).json(post)
     }catch(err) {
-        console.log(err);
         res.status(404).json({err})
     }
 }
-// async function createNewPosts(req, res) {
-//   console.log("Hitting post request on Controllers");
-//   try {
-//     console.log(req.body);
-//     // const { title, name, body } = req.body;
-//     let newPost = await Post.insertMoreThanPost(req.body);
-//     res.status(201).send(newPost);
-//   } catch (err) {
-//     console.log(err);
-//     res.status(400).json({err})
-//   }
-// }
 module.exports = { createNewPost, findById, getAllPosts }
--- server/models/post.js
+++ server/models/post.js
@@ -12,17 +12,13 @@ class Post {
   static all() {
     return new Promise(async (resolve, reject) => {
       try {
-        console.log("*********\n getting all posts models");
         const db = await init();
         const postsData = await db.collection("post").find().toArray();
-        console.log(postsData);
         let posts = postsData.map(
           (post) => new Post({ ...post, id: post._id })
         );
-        console.log("******trying to map the new array\n", posts);
         resolve(posts);
       } catch (err) {
-        console.log(err);
         reject("empty list");
       }
     });
@@ -31,13 +27,11 @@ class Post {
   static findById(id) {
     return new Promise(async (resolve, reject) => {
       try {
-        console.log("*********\n findBYId function in models");
         const db = await init();
         let postData = await db
           .collection("posts")
           .find({ _id: ObjectId(id) })
           .toArray();
-        console.log(postData);
         let post = new Post({ ...postData[0], id: postData[0]._id });
         resolve(post);
       } catch (err) {
@@ -49,44 +43,18 @@ class Post {
   static create(title, name, body) {
     return new Promise(async (resolve, reject) => {
       try {
-        console.log("**************\n create function in modles");
         let postData = { title, name, body };
         const db = await init();
         postData._id = (
           await db.collection("posts").insertOne(postData)
         ).insertedId;
-        console.log("*****Inserted new post to db*****\n", postData);
-        console.log(postData._id, typeof postData._id);
-        // const post = await db.collection('posts).find
         let newPost = new Post(postData);
         resolve(newPost);
       } catch (err) {
-        console.log(err);
         reject("Error creating post");
       }
     });
   }
-
-  // static insertMoreThanPost(array) {
-  //   return new Promise(async (resolve, reject) => {
-  //     try {
-  //       console.log("**************\n create function in modles");
-  //       // let postData = { title, name, body }
-  //       const db = await init();
-  //       const insertedData = await db
-  //         .collection("test")
-  //         .insertMany(array)
-  //         console.log("*****Inserted new post to db*****\n",insertedData);
-  //         console.log("looking for results",insertedData.insertedIds);
-  //         // const post = await db.collection('posts).find
-  //       let newPost = new Post(postData);
-  //       resolve(newPost);
-  //     } catch (err) {
-  //       console.log(err);
-  //       reject("Error creating post");
-  //     }
-  //   });
-  // }
 }
 
 module.exports = Post;
